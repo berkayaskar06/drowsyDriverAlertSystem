3faaed157a28b51ff8675fdbaf90211a5dc6a992
berkayaskar06 <53000486+berkayaskar06@users.noreply.github.com>
2020-12-17 16:30:47 +0300

    Update Drowsy_Driver.py

--- Drowsy_Driver.py
+++ Drowsy_Driver.py
@@ -1,64 +1,41 @@
-
-#Kütüphaneler
+#Libraries
 from scipy.spatial import distance as dist
-from imutils.video import VideoStream
 from imutils import face_utils
-from threading import Thread
 import numpy as np
-import imutils
 import time
 import dlib
 import cv2
-import os
-
 
 def alarm(msg):
     global alarm_status
     global alarm_status2
     global saying
-
     while alarm_status:
-        print('call')
-
-
-    if alarm_status2:
-        print('call')
-        saying = True
-        saying = False
-
-
+        if alarm_status2:
+            saying = True
+            saying = False
 def eye_aspect_ratio(eye):
     A = dist.euclidean(eye[1], eye[5])
     B = dist.euclidean(eye[2], eye[4])
-
     C = dist.euclidean(eye[0], eye[3])
-    #EAR Hesabı
     ear = (A + B) / (2.0 * C)
-
     return ear
-
-#defining eyes location
+#defining eyes locations
 def final_ear(shape):
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-
     leftEye = shape[lStart:lEnd]
     rightEye = shape[rStart:rEnd]
-
     leftEAR = eye_aspect_ratio(leftEye)
     rightEAR = eye_aspect_ratio(rightEye)
-
     ear = (leftEAR + rightEAR) / 2.0
     return (ear, leftEye, rightEye)
-
 
 def lip_distance(shape):
     top_lip = shape[50:53]
     top_lip = np.concatenate((top_lip, shape[61:64]))
-
     low_lip = shape[56:59]
     low_lip = np.concatenate((low_lip, shape[65:68]))
-
     top_mean = np.mean(top_lip, axis=0)
     low_mean = np.mean(low_lip, axis=0)
 
@@ -66,15 +43,17 @@ def lip_distance(shape):
     return distance
 
 
-
-
-EYE_AR_THRESH = 0.25 #Göz Açıklık Thresh
-EYE_AR_CONSEC_FRAMES = 30 #kapalı kalınan süre
-YAWN_THRESH = 15 #esneme Thresh 
+foo_counter = 0
+EYE_AR=0
+EYE_AR_THRESH = 0
+EYE_AR_CONSEC_FRAMES_DROWSY = 30 #Kapalı kalan Frame Sayısı
+EYE_AR_CONSEC_FRAMES_BLINK = 3 #Kapalı kalan Frame Sayısı
+YAWN_THRESH = 0
 alarm_status = False
 alarm_status2 = False
 saying = False
 COUNTER = 0
+TOTAL = 0
 
 print("-> Loading the predictor and detector...")
 detector = dlib.get_frontal_face_detector()
@@ -82,21 +61,25 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("C:\shape_predictor_68_face_landmarks.dat")
 
 print("-> Starting Video Stream")
-vs = cv2.VideoCapture(0)
+vs = cv2.VideoCapture("Video_2.mp4")
 
+result = cv2.VideoWriter('filename.avi',
+                             cv2.VideoWriter_fourcc(*'MJPG'), 30
+                             , (450, 600))
+print("-> Calculation eye aspect ratio keep your eyes open:")
+start_time = time.time()
 while True:
+
+    foo_counter = foo_counter+1
+    end_time = time.time()
+    duration = int(end_time - start_time)
     start = time.time()
     ret,frame = vs.read()
-    frame = imutils.resize(frame, width=450)
-    #frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
+    frame = cv2.resize(frame,(600,450))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     rects = detector(gray, 0)
 
-
     for rect in rects:
-
-
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
 
@@ -106,6 +89,10 @@ while True:
         rightEye = eye[2]
 
         distance = lip_distance(shape)
+        if foo_counter <2:
+            EYE_AR_THRESH = ear*70/100
+            YAWN_THRESH = 48
+
 
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
@@ -118,15 +105,18 @@ while True:
         if ear < EYE_AR_THRESH:
             COUNTER += 1
 
-            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+            if COUNTER >= EYE_AR_CONSEC_FRAMES_DROWSY:
                 if alarm_status == False:
                     alarm_status = True
-                cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+                cv2.putText(frame, "DROWSINESS ALERT!", (10, 60),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
+
         else:
-            COUNTER = 0
-            alarm_status = False
+            if COUNTER>=EYE_AR_CONSEC_FRAMES_BLINK:
+                TOTAL += 1
+                COUNTER = 0
+                alarm_status = False
 
         if (distance > YAWN_THRESH):
             cv2.putText(frame, "Yawn Alert", (10, 30),
@@ -135,10 +125,15 @@ while True:
                 alarm_status2 = True
         else:
             alarm_status2 = False
-        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (450, 30),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, "YAWN: {:.2f}".format(distance), (300, 60),
+        cv2.putText(frame, "YAWN: {:.2f}".format(distance), (450, 60),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "Time: {}".format(duration),(10,450),
+                    cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        cv2.putText(frame, "Blinks: {}".format(TOTAL),(450,90),
+                    cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+    result.write(frame)
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
@@ -148,7 +143,10 @@ while True:
     time1 = end-start
 
 
-
+    cv2.putText(frame, "Time: {:.2f}".format(time1),(250,250),
+            cv2.FONT_HERSHEY_COMPLEX, 0.7,(0,0,255),2)
 vs.release()
-print(time1)
+result.release()
+print('Video was Saved')
+
 cv2.destroyAllWindows()
